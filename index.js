a55f72b816c188ca0bf57de79ded3f5d3dd4b485
asad-ac <asadachau@gmail.com>
2025-06-11 20:03:40 -0400

    got a lot of html done

--- index.js
+++ index.js
@@ -2,4 +2,5 @@ const darkModeBtn =  document.getElementById('dark-mode-btn')
 
 darkModeBtn.addEventListener('click',  function(){
    document.body.style.backgroundColor = '#121212'
+   document.body.style.color = 'white'
 })
